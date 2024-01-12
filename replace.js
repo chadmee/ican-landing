1fde48046a3a3feb8b3cce7650b57a1353811d4b
chadmee <chadmexdlul@gmail.com>
2024-01-12 23:35:04 +0100

    Fixed positioning of elements, re-worked responsivity for mobile devices and added a javascript file for replacing html content within shoptet

--- replace.js
+++ replace.js
@@ -1,8 +1,9 @@
-// Read the content of your HTML file
+// Fetches the html file
 fetch('https://www.icandrink.wtf/user/documents/upload/index.html')
   .then(response => response.text())
   .then(htmlContent => {
-    // Replace the entire content of the current HTML file
+    // Replace all html data with ones from file
     document.documentElement.innerHTML = htmlContent;
   })
+  // In case anything fucks up (shoptet bruh)
   .catch(error => console.error('Error loading HTML file:', error));
