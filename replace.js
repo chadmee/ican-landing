b4694c6031f86c4d8119d13ae820945c12251ab5
chadmee <chadmexdlul@gmail.com>
2024-03-12 17:15:51 +0100

    Wrapped into function

--- replace.js
+++ replace.js
@@ -1,13 +1,9 @@
 // Fetches the html file
-fetch('https://www.icandrink.wtf/user/documents/upload/index.html')
+fetch('https://www.icandrink.wtf/user/documents/upload/test.html')
   .then(response => response.text())
   .then(htmlContent => {
     // Replace all html data with ones from file
     document.documentElement.innerHTML = htmlContent;
   })
-  .then(getScript('https://www.icandrink.wtf/user/documents/upload/script.js'))
   // In case anything fucks up (shoptet bruh)
   .catch(error => console.error('Error loading HTML file:', error));
-
-
-  $(document).ready(function)
