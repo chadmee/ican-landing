0eaf05217b5d9d7fb55e2b2cb3907f47881e5240
chadmee <chadmexdlul@gmail.com>
2024-03-12 17:37:20 +0100

    Reworked the replacement script

--- replace.js
+++ replace.js
@@ -1,9 +1,14 @@
 // Fetches the html file
-fetch('https://www.icandrink.wtf/user/documents/upload/test.html')
+fetch('https://www.icandrink.wtf/user/documents/upload/testujeme.html')
   .then(response => response.text())
   .then(htmlContent => {
     // Replace all html data with ones from file
     document.documentElement.innerHTML = htmlContent;
+
+    // Load and run a script from a URL
+    const script = document.createElement('script');
+    script.src = 'https://www.icandrink.wtf/user/documents/upload/change.js';
+    document.body.appendChild(script);
   })
-  // In case anything fucks up (shoptet bruh)
+  // In case anything goes wrong
   .catch(error => console.error('Error loading HTML file:', error));
--- script.js
+++ script.js
@@ -1,34 +1,29 @@
 let $img = document.querySelector('.landBanner');
-let $text = document.querySelector('.landSecondary');
-var i = 0;
-var source = ["https://www.icandrink.wtf/user/documents/upload/Landing Obsah/v2/ican_parkoviste-1-2.jpg","https://www.icandrink.wtf/user/documents/upload/Landing Obsah/v2/ican_parkoviste-5-2.jpg","https://www.icandrink.wtf/user/documents/upload/Landing Obsah/v2/ican_parkoviste-2-2.jpg"];
-var textContent = ["Taste the freedom.\n\nYou can!", "Can you feel it? That unmistakable ambiance?\nThat singular, defining moment. It's the synergy of elements, a seamless fusion. Here, tradition meets innovation, intertwining two distinct cultures and communities.\nIt's about bringing together cannabis and hops, two kindred plants, to craft a perfect moment, here and now. Cannabis?\nThey told us we cannot. But we believe we can.", "True school craft beer.\n\nIndeed."]
-document.onload = $img.attr("src", source[0]),$text.text("Taste the freedom.\n\nYou can!");
+    let $text = document.querySelector('.landSecondary');
+    var i = 0;
+    var source = ["https://www.icandrink.wtf/user/documents/upload/Landing Obsah/v2/ican_parkoviste-1-2.jpg","https://www.icandrink.wtf/user/documents/upload/Landing Obsah/v2/ican_parkoviste-5-2.jpg","https://www.icandrink.wtf/user/documents/upload/Landing Obsah/v2/ican_parkoviste-2-2.jpg"];
+    var textContent = ["Taste the freedom.\nYou can!", "Can you feel it? That unmistakable ambiance?\nThat singular, defining moment. It's the synergy of elements, a seamless fusion. Here, tradition meets innovation, intertwining two distinct cultures and communities.\nIt's about bringing together cannabis and hops, two kindred plants, to craft a perfect moment, here and now. Cannabis?\nThey told us we cannot. But we believe we can.", "True school craft beer.\nIndeed."]
 
-function changeContent() {
+    function icanRoll() {
     let repeat = setInterval(function () {
+        if (i > 2) {
+        i = 0;
+        }
+
         let todayDate = new Date().getTime();
-        $text.fadeOut(1200);
-        $img.fadeOut(1000);
-    
-        $text.promise().done(function () {
-            $text.textContent(textContent[i]);
-            $img.src = source[i];
-            $img.fadeIn(1000);
-            $text.fadeIn(1500);
-        
-            ++i;
-    
-            if (i >= 3) {
-                i = 0;
-            }
+        $(".landSecondary").fadeOut(1200);
+        $(".landBanner").fadeOut(1000);
 
-            if ($img.attr("src") != source[0] && $img.attr("src") != source[1] && $img.attr("src") != source[2]) {
-                $text.text("Vylilo se nám pivko a zamazalo kód.\nOmlouváme se za komplikace.");
-            }
-        });
+        $(".landBanner").promise().done(function () {
+        $text.innerText = textContent[i];
+        $img.src = source[i];
+        $(".landBanner").fadeIn(1000);
+        $(".landSecondary").fadeIn(1500);
 
-    }, 5000);
-}
+        ++i;
+    });
 
-changeContent();
+    }, 8000);
+    }
+
+    document.onload = $img.src = source[1], $text.innerHTML = "Taste the freedom.\nYou can!", icanRoll();
